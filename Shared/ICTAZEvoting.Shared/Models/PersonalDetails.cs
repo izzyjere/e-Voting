c23c2305b212fc61154d296a69a37af8b2e0159f
izzyjere <wisdomjere5@gmail.com>
2022-07-07 15:00:43 +0200

    NRC Regex

--- Shared/ICTAZEvoting.Shared/Models/PersonalDetails.cs
+++ Shared/ICTAZEvoting.Shared/Models/PersonalDetails.cs
@@ -1,5 +1,6 @@
 ﻿using ICTAZEvoting.Shared.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ICTAZEvoting.Shared.Models
 {
@@ -8,11 +9,14 @@ namespace ICTAZEvoting.Shared.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? MiddleName { get; set; }
+        [Required,RegularExpression(@"^\d{6}\/\d{2}\/\d{1}$", ErrorMessage ="Please enter a valid NRC. xxxxxx/xx/x")]
         public string NRC { get; set; }
         public string PhoneNumber { get; set; }
         public Gender Gender { get; set; }
         public string PictureUrl { get; set; }
         public DateTime DateOfBirth { get; set; }         
         public string Address { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }

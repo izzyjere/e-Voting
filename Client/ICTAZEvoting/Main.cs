233c0047c48dd9e473a1c6bbb47eb16ef3f3284a
izzyjere <wisdomjere5@gmail.com>
2022-07-07 17:54:23 +0200

    mudblazor works

--- Client/ICTAZEvoting/Main.cs
+++ Client/ICTAZEvoting/Main.cs
@@ -1,6 +1,9 @@
-using ICTAZEvoting.Pages;
+using ICTAZEvoting.WebUI;
+
 using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 using Microsoft.Extensions.DependencyInjection;
+
+using MudBlazor.Services;
 
 namespace ICTAZEVoting
 {
@@ -10,10 +13,11 @@ namespace ICTAZEVoting
         {
             InitializeComponent();
             var services = new ServiceCollection();
+            services.AddMudServices();
             services.AddWindowsFormsBlazorWebView();
             blazorWebView1.HostPage = "wwwroot\\index.html";
             blazorWebView1.Services = services.BuildServiceProvider();
-            blazorWebView1.RootComponents.Add<Counter>("#app");
+            blazorWebView1.RootComponents.Add<App>("#app");
         }
 
         private void blazorWebView1_Click(object sender, EventArgs e)

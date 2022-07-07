5dbbf0b209241e2dcb07779c5ef72e04e73a08e3
izzyjere <wisdomjere5@gmail.com>
2022-07-07 18:57:33 +0200

    added core extensions

---
+++ Core/ICTAZEVoting.Core/DatabaseSeeder.cs
@@ -0,0 +1,18 @@
+﻿using ICTAZEvoting.Shared.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICTAZEVoting.Core
+{
+    public class DatabaseSeeder : ISeeder
+    {
+        public void Seed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/ICTAZEVoting.Core/Extensions/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using ICTAZEVoting.Core.Data.Contexts;
+using ICTAZEVoting.Core.Data.Repositories;
+using ICTAZEVoting.Core.Services.Identity;
+using ICTAZEvoting.Shared.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICTAZEVoting.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services
+                    .AddTransient<IUnitOfWork<int>, UnitOfWork<int>>()
+                    .AddTransient<ISeeder, DatabaseSeeder>();
+
+            return services;
+        }
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserAccountService, UserAccountService>()
+                    .AddScoped<IRoleService, RoleService>()
+                    .AddScoped<IUserService, UserService>()
+                    .AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+        public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            services
+                .AddHttpContextAccessor()
+                .AddScoped<ICurrentUserService, CurrentUserService>()
+                .AddDbContext<SystemDbContext>(optionsBuilder =>
+                {
+                    optionsBuilder.UseSqlServer(connectionString, options =>
+                    {
+                        options.MigrationsAssembly(typeof(SystemDbContext).Assembly.FullName);
+                        options.EnableRetryOnFailure();
+                        options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    });
+                }, ServiceLifetime.Transient);
+
+            return services;
+        }
+    }
+}

9cc56174d5ca9ebec87946cc4be3915d177a551c
izzyjere <wisdomjere5@gmail.com>
2022-07-07 14:28:46 +0200

    doc

--- Shared/ICTAZEvoting.Shared/Models/BlockChain.cs
+++ Shared/ICTAZEvoting.Shared/Models/BlockChain.cs
@@ -17,6 +17,10 @@ namespace ICTAZEvoting.Shared.Models
         {
             Chain = new List<Block>();
         }
+        /// <summary>
+        /// This method creates the genesis
+        /// </summary>
+        /// <returns></returns>
         public Block CreateGenesisBlock()
         {
             return new Block(DateTime.Now, "",null);
@@ -29,10 +33,19 @@ namespace ICTAZEvoting.Shared.Models
         {
             return Chain[Chain.Count-1];
         }
+        /// <summary>
+        /// Counts the votes for a particular candidate
+        /// </summary>
+        /// <param name="candidateId"></param>
+        /// <returns></returns>
         public int GetVotesCount(Guid candidateId)
         {
            return Chain.Count(b=>b.Data.CandidateId==candidateId); 
         }
+        /// <summary>
+        /// Adds a new block to the chain.
+        /// </summary>
+        /// <param name="block"></param>
         public void AddBlock(Block block)
         {
             Block latestBlock = GetLatestBlock();
@@ -41,6 +54,12 @@ namespace ICTAZEvoting.Shared.Models
             block.Hash = block.CalculateHash();
             Chain.Add(block);
         }
+        /// <summary>
+        ///    The IsValid method will check two things.
+        ///    Each block’s hash to see if the block is changed
+        ///    Previous block’s hash to see if the block is changed and recalculated
+        /// </summary>
+        /// <returns>true if the block is valid.</returns>
         public bool IsValid()
         {
             for (int i = 1; i < Chain.Count; i++)

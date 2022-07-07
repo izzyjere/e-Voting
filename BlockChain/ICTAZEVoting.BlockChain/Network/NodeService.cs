f4ef726aea7c0acaa08d6549184d3d0a5fe3a506
izzyjere <wisdomjere5@gmail.com>
2022-07-07 23:23:26 +0200

    network config

--- BlockChain/ICTAZEVoting.BlockChain/Network/NodeService.cs
+++ BlockChain/ICTAZEVoting.BlockChain/Network/NodeService.cs
@@ -6,13 +6,19 @@ namespace ICTAZEVoting.BlockChain.Network
 {
     public static class NodeService
     {
-        public static Dictionary<string, WebSocket> Nodes { get; set; }
+        public static Dictionary<string, WebSocket> Nodes { get; }
         public static void Add(string nodeAddress, WebSocket server)
         {
-            if(!Nodes.ContainsKey(nodeAddress))
+            if(!Nodes.ContainsKey(nodeAddress)|| !nodeAddress.Equals(NodeInstance.IPAddress))
                 Nodes.Add(nodeAddress, server);
         }
-        public static StorageContext  Storage { get; set; }
-        public static Node NodeInstance { get; set; }
+        public static StorageContext  Storage { get; private set; }
+        public static Node NodeInstance { get; private set; }
+        public static void InitializeNode(string storagePath, string IpAddress, int portNumber)
+        {
+            NodeInstance = new Node { IPAddress = IpAddress, Port = portNumber, NodeId = Guid.NewGuid() };
+            //:TODO  Register node with the network
+
+        }
     }
 }

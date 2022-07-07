e158ea09b842a1352355f69ca0a26fe4e045df85
izzyjere <wisdomjere5@gmail.com>
2022-07-07 11:23:26 +0200

    contracts

---
+++ Shared/ICTAZEvoting.Shared/Wrapper/IResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICTAZEvoting.Shared.Wrapper
+{
+    public interface IResult
+    {
+        List<string> Messages { get; set; }
+
+        bool Succeeded { get; set; }
+    }
+
+    public interface IResult<out T> : IResult
+    {
+        T Data { get; }
+    }
+
+}

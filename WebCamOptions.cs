82ac2aedc7100151f146810ce9d4f267adc32d54
Clarence Simukanzye <clarenceclanz2@gmail.com>
2022-07-09 11:12:29 +0200

    Capture Passport Size Photo for voter registration

---
+++ WebCamOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ICTAZEvoting
+{
+    public class WebCamOptions
+    {
+        public int Width { get; set; } = 320;
+        public string VideoID { get; set; }
+        public string CanvasID { get; set; }
+        public string Filter { get; set; } = null;
+    }
+}

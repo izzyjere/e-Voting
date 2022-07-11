dd5a03fa1ca90c60791e957eb195d69cc20c5762
izzyjere <wisdomjere5@gmail.com>
2022-07-11 07:31:30 +0200

    register services

---
+++ Client/ICTAZEVoting/Main.cs
@@ -0,0 +1,67 @@
+using ICTAZEVoting.Extensions;
+using ICTAZEVoting.Services.Domain;
+using ICTAZEVoting.Services.Identity;
+using ICTAZEVoting.Shared.Interfaces;
+using ICTAZEVoting.WebUI;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using System.Globalization;
+using System.Net.Http;
+
+namespace ICTAZEVoting
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            Task.Run(async()=> await SessionStorage.RemoveItemAsync("UserToken"));
+            var services = new ServiceCollection();
+
+            services.AddMudServices(configuration =>
+            {
+                configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                configuration.SnackbarConfiguration.HideTransitionDuration = 1000;
+                configuration.SnackbarConfiguration.ShowTransitionDuration = 100;
+                configuration.SnackbarConfiguration.VisibleStateDuration = 5000;
+                configuration.SnackbarConfiguration.NewestOnTop = true;
+                configuration.SnackbarConfiguration.MaximumOpacity = 100;
+                configuration.SnackbarConfiguration.ShowCloseIcon = true;
+            });
+            services.AddTransient<AuthenticationHeaderHandler>()
+                 .AddScoped(sp => sp
+                   .GetRequiredService<IHttpClientFactory>()
+                   .CreateClient("ICTAZEVotingSystem"))
+               .AddHttpClient("ICTAZEVotingSystem", client =>
+               {
+                   client.DefaultRequestHeaders.AcceptLanguage.Clear();
+                   client.DefaultRequestHeaders.AcceptLanguage.ParseAdd(CultureInfo.DefaultThreadCurrentCulture?.TwoLetterISOLanguageName);
+                   client.BaseAddress = new Uri("https://localhost:7119");
+               })
+               .AddHttpMessageHandler<AuthenticationHeaderHandler>();
+            services.AddScoped<IVoterService, VoterService>();
+            services.AddScoped<IVotingService, VotingService>();
+            services.AddScoped<IElectionService, ElectionService>();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            services.AddAuthorizationCore()
+            .AddScoped<IAuthenticationService, AuthenticationService>();       
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<App>("#app");
+        }
+
+        private void blazorWebView1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

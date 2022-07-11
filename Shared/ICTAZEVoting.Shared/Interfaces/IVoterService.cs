10cae5b7f63ed37357ce2b4d663b34b99926f825
izzyjere <wisdomjere5@gmail.com>
2022-07-11 07:18:55 +0200

    client interfaces

---
+++ Shared/ICTAZEVoting.Shared/Interfaces/IVoterService.cs
@@ -0,0 +1,21 @@
+﻿using ICTAZEVoting.Shared.Models;
+using ICTAZEVoting.Shared.Wrapper;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICTAZEVoting.Shared.Interfaces
+{
+    public interface IVoterService
+    {
+        Task<IResult<string>> Register(Voter entity);
+        Task<IResult> Update(Voter entity);
+        Task<List<Voter>> GetAll();
+        Task<Voter>GetByUserId(string id);
+        Task<Voter> GetById(string id);
+        Task<IResult> Delete(Voter entity);
+        Task<IResult> VerifyVoter(byte[] facialData);
+    }
+}

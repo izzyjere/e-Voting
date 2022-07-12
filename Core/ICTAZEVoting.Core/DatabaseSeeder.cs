8cc256ff241e9302aa01ee69a63e787be82fc09e
izzyjere <wisdomjere5@gmail.com>
2022-07-12 06:20:03 +0200

    register services

--- Core/ICTAZEVoting.Core/DatabaseSeeder.cs
+++ Core/ICTAZEVoting.Core/DatabaseSeeder.cs
@@ -37,7 +37,7 @@ namespace ICTAZEVoting.Core
         }
         private void AddAdministrator()
         {
-            Task.Run(async () =>
+            dminTask.Run(async () =>
             {
                 //Check if Role Exists
                 var adminRole = new Role

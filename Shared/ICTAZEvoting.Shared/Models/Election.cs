93053738fd4221019294e23c60aec08ced31f862
izzyjere <wisdomjere5@gmail.com>
2022-07-15 22:26:04 +0200

    new migration

--- Shared/ICTAZEvoting.Shared/Models/Election.cs
+++ Shared/ICTAZEvoting.Shared/Models/Election.cs
@@ -19,11 +19,28 @@ namespace ICTAZEVoting.Shared.Models
         public Guid ElectionTypeId { get; set; }
         [Required,Range(1,48)]
         public double Duration { get; set; }
-        public ElectionStatus Status { get; set; } 
+        public ElectionStatus Status {
+            get
+            {
+                if(ClosingTime<= DateTime.Now)
+                {
+                    return ElectionStatus.Closed;
+                }
+                else if(ClosingTime>= DateTime.Now && ElectionDate<=DateTime.Now)
+                {
+                    return ElectionStatus.Open;
+                }
+                else
+                {
+                    return ElectionStatus.Pending;
+                }
+            }
+        }
+        public bool IsCurrent { get; set; }
         public List<ElectionVoter> Voters { get; set; }
         public List<ElectionPosition> Positions { get; set; }
         public DateTime ClosingTime => ElectionDate.Value.AddHours(Duration);
-        public ElectionType Type { get; set; }
+        public ElectionType Type { get; set; }         
         public Election()
         {
             Id = Guid.NewGuid();

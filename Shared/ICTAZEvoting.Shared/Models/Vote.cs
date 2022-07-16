945916cfe5a114b1a4415b70c2374eef927ee04e
izzyjere <wisdomjere5@gmail.com>
2022-07-16 10:07:20 +0200

    ballot finish ups

--- Shared/ICTAZEvoting.Shared/Models/Vote.cs
+++ Shared/ICTAZEvoting.Shared/Models/Vote.cs
@@ -5,12 +5,7 @@ namespace ICTAZEVoting.Shared.Models
 {
     public class Vote 
     {
-        public DateTime TimeStamp { get; set; }
-        public Guid PositionId { get; set; }      
-        public Guid PollingStationId { get; set; }
-        public string NodeAddress { get; set; }
+        public Guid PositionId { get; set; }    
         public Guid CandidateId { get; set; }  
-
-
     }
 }
